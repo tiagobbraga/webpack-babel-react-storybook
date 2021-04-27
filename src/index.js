f62c632612c6e1518892de64b2f369feef12eeaf
Tiago Braga <contato@tiagobraga.cc>
2021-04-27 11:55:18 -0300

    run react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-(function helloWorld() {
-  console.log("hello world");
-})();
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.css";
+
+function HelloWorld(props) {
+  return <h1>Hello World again</h1>;
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,23 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, "src"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
