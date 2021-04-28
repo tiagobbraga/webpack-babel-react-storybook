d2add07780e5a6471dde38e659bebae4ed9a2b0f
Tiago Braga <contato@tiagobraga.cc>
2021-04-28 11:39:59 -0300

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const custom = require("../webpack.config");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: custom.module.rules,
+      },
+    };
+  },
+};
